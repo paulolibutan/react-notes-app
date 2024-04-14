fef9b29a2b2fa2bf54c8b485695865faa4c5eef3
libutanpaulo <libutanpaulo@gmail.com>
2024-04-14 20:42:44 +0800

    Add note and notelist components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import NotesList from "./components/NotesList";
+import { nanoid } from "nanoid";
 
 const App = () => {
-  return <div>Hello World!</div>;
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This is my first note",
+      date: "14/04/2024",
+    },
+    {
+      id: nanoid(),
+      text: "This is my second note",
+      date: "13/04/2024",
+    },
+    {
+      id: nanoid(),
+      text: "This is my third note",
+      date: "10/04/2024",
+    },
+  ]);
+
+  return (
+    <div className="container">
+      <NotesList notes={notes}/>
+    </div>
+  );
 };
 
 export default App;
