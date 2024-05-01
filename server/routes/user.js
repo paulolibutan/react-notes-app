35b76af4fb248cf275fe993c152999cde9dc3d7e
Paulo Libutan <libutanpaulo@gmail.com>
2024-05-01 22:57:57 +0800

    Add get user by id route and controller

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require("../controllers/user");
+const { verify } = require("../middlewares/auth.js");
 
+router.get("/details", verify, userController.getUserById);
 router.post("/register", userController.registerUser);
 router.post("/login", userController.loginUser);
 
