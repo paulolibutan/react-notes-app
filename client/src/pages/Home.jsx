443ff8e462d8cce2ee62ee5514463ef212048781
Paulo Libutan <libutanpaulo@gmail.com>
2024-05-16 00:58:28 +0800

    Update login and home functionalities

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,39 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { FaPlus } from "react-icons/fa6";
 import { Navigate } from "react-router-dom";
 
 import AuthContext from "../context/AuthContext";
 import Navbar from "../components/Navbar";
+import CreateNote from "../components/CreateNote";
+import MobileNav from "../components/MobileNav";
 const Home = () => {
-  const { isAuthenticated } = useContext(AuthContext);
+  const { isAuthenticated, logout } = useContext(AuthContext);
+  const [isOpen, setIsOpen] = useState(false);
+  const [showMobileNav, setShowMobileNav] = useState(false);
+
+  const handleOpenAddNote = () => {
+    setIsOpen(true);
+  };
+
+  const handleLogout = () => {
+    logout();
+  };
 
   return isAuthenticated ? (
     <div>
-      <Navbar />
-      <h1>This is the homepage</h1>
+      <Navbar
+        handleLogout={handleLogout}
+        showMobileNav={showMobileNav}
+        setShowMobileNav={setShowMobileNav}
+      />
+      {showMobileNav && <MobileNav handleLogout={handleLogout} />}
+      <CreateNote isOpen={isOpen} />
+      <button
+        onClick={handleOpenAddNote}
+        className="fixed bottom-10 right-10 bg-emerald-500 p-3 rounded-full text-2xl text-white shadow-2xl"
+      >
+        <FaPlus />
+      </button>
     </div>
   ) : (
     <Navigate to="/login" />
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,12 @@
 import { useContext, useState } from "react";
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import AuthContext from "../context/AuthContext";
 
 const Login = () => {
   const baseUrl = import.meta.env.VITE_API_BASE_URL;
-  const navigate = useNavigate();
   const { login, isAuthenticated } = useContext(AuthContext);
 
   const [formData, setFormData] = useState({
@@ -36,64 +37,87 @@ const Login = () => {
     })
       .then((res) => res.json())
       .then((data) => {
+        if (data.message === "Email or password is invalid") {
+          toast.error(data.message, {
+            theme: "colored",
+          });
+        }
+
+        if (data.error) {
+          toast.error(data.error, {
+            theme: "colored",
+          });
+        }
+
         if (typeof data.accessToken !== "undefined") {
           login(data.accessToken);
-          navigate("/");
         }
       });
+    setFormData({
+      ...formData,
+      email: "",
+      password: "",
+    });
   };
+
+  console.log(formData);
 
   return isAuthenticated ? (
     <Navigate to="/" />
   ) : (
-    <div className="flex justify-center items-center h-screen">
-      <div className="flex flex-col border min-w-[25%] p-10 mt-5 shadow-xl">
-        <h1 className="text-2xl font-bold text-center">Login to Noteefy</h1>
-        <form onSubmit={(e) => handleLogin(e)}>
-          <div className="flex flex-col justify-center mt-5">
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              onChange={(e) => handleInputChange(e)}
-              placeholder="Enter your email"
-              className="border rounded-sm w-full px-3 py-1"
-            />
-          </div>
+    <>
+      <ToastContainer autoClose={2500} pauseOnHover={false} />
+      <div className="flex justify-center items-center h-screen">
+        <div className="flex flex-col border min-w-[25%] p-10 mt-5 shadow-xl">
+          <h1 className="text-2xl font-bold text-center">Login to Noteefy</h1>
+          <form onSubmit={(e) => handleLogin(e)}>
+            <div className="flex flex-col justify-center mt-5">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={(e) => handleInputChange(e)}
+                placeholder="Enter your email"
+                className="border rounded-sm w-full px-3 py-1"
+              />
+            </div>
 
-          <div className="flex flex-col justify-center mt-5">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Enter your password"
-              onChange={(e) => handleInputChange(e)}
-              className="border rounded-sm w-full px-3 py-1"
-            />
-          </div>
+            <div className="flex flex-col justify-center mt-5">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={(e) => handleInputChange(e)}
+                placeholder="Enter your password"
+                className="border rounded-sm w-full px-3 py-1"
+              />
+            </div>
 
-          <div className="flex flex-col justify-center mt-5">
-            <button className="bg-emerald-500 hover:bg-emerald-600 text-white py-2 w-full">
-              Login
-            </button>
-          </div>
+            <div className="flex flex-col justify-center mt-5">
+              <button
+                type="submit"
+                className="bg-emerald-500 hover:bg-emerald-600 text-white py-2 w-full"
+              >
+                Login
+              </button>
+            </div>
 
-          <div
-            type="submit"
-            className="flex flex-col justify-center mt-3 text-sm"
-          >
-            <span>
-              No account yet?{" "}
-              <Link to="/register" className="underline underline-offset-2">
-                Sign up here
-              </Link>
-            </span>
-          </div>
-        </form>
+            <div className="flex flex-col justify-center mt-3 text-sm">
+              <span>
+                No account yet?{" "}
+                <Link to="/register" className="underline underline-offset-2">
+                  Sign up here
+                </Link>
+              </span>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
