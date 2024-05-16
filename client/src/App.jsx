a9f81e4876b11d1a7ea28c01a2aa838be8958fee
Paulo Libutan <libutanpaulo@gmail.com>
2024-05-16 22:29:53 +0800

    Started from scratch to use flowbite-react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import { AuthProvider } from "./context/AuthContext";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import { Button } from "flowbite-react";
 
 const App = () => {
-  return (
-    <Router>
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </AuthProvider>
-    </Router>
-  );
+  return <Button gradientDuoTone="greenToBlue">Green to Blue</Button>;
 };
 
 export default App;
